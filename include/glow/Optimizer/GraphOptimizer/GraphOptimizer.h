1390bf24e7a23792ab8eacc9b7071138eb7c0179
Roman Levenstein <rlevenstein@fb.com>
2019-06-21 17:54:09 -0700

    [graph-optimizer] Implement general constant folding
    Any non-trivial node (i.e. not a constant or a splat) that can be computed at compile-time is going to be computed at compile-time! This is implemented by detecting nodes with constant inputs and evaluating their results at compile-time by means of using a temporary interpreter-based execution engine.

--- include/glow/Optimizer/GraphOptimizer/GraphOptimizer.h
+++ include/glow/Optimizer/GraphOptimizer/GraphOptimizer.h
@@ -69,6 +69,20 @@ llvm::Error optimizeFunction(Function *F, const Backend &B,
 /// compiler error.
 llvm::Error optimizeFunctionBeforeLowering(Function *F,
                                            CompilationContext &cctx);
+
+/// Perform a compile-time constant folding of the node \p N.
+/// \returns list of constants which are the result of the constant-folding.
+/// These constants correspond to results of the node. If no constant folding
+/// was possible an empty vector will be returned.
+std::vector<Constant *> constantFold(Node *N);
+
+/// Execute function \p F by the \p backend using the provided \p bindings and
+/// the compilation context \p cctx. If \p isConstant is true, the function
+/// should consist of constant operations only. \returns error if the function
+/// contains non-constant nodes in spite of \p isConstant being true.
+llvm::Error executeFunction(Backend &backend, Function &F,
+                            PlaceholderBindings &bindings,
+                            CompilationContext &cctx, bool isConstant);
 } // namespace glow
 
 #endif // GLOW_OPTIMIZER_GRAPHOPTIMIZER_GRAPHOPTIMIZER_H
